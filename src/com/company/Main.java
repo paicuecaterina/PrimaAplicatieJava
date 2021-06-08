9c342454ff002d872529326a49a2ed542d17d6b5
paicuecaterina <paicu.ecaterina@yahoo.com>
2021-06-08 14:45:50 -0700

    Am adaugat Java Doc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,15 @@ package com.company;
 
 public class Main {
 
-    //Aceasta este metoda main care porneste o aplicatie Java
-    /*
-    cgvhjbknlm
-    kjhjgjkl;
+    /**
+     *Metoda main este metoda apelata de masina virtuala Java si care porneste o aplicatie Java
+     *Putem sa transmitem argumente aditionale prin parametrul <b>args</b>
+     *  @param args Acesta este argumentul cu care transmitem argumente programului nostru
+     *@author Paicu Ecaterina
      */
-    public static void main(/*guigjjjkj*/ String[] args) {
-        /* fdbdbfggb
-        jbknlmkjvhjbknlm
-        nklm;,njklm;
-        joikplk
-         */
+
+    public static void main(String[] args) {
+
 	        System.out.println("Invat limbajul Java!");
     }
 }
