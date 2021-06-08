4c74ee3b9e372a5851987bd676862eb70180fb09
paicuecaterina <paicu.ecaterina@yahoo.com>
2021-06-08 13:51:53 -0700

    Adaugare comentarii pe o singura linie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,10 @@ package com.company;
 
 public class Main {
 
+    //Aceasta este metoda main care porneste o aplicatie Java
+
     public static void main(String[] args) {
+
 	        System.out.println("Invat limbajul Java!");
     }
 }
